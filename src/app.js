06326000bf572389a846013221d0b36ce271f486
Faisal Bahari <fbhr1112@gmail.com>
2024-03-17 18:53:29 +0700

    fix(route): prefix route

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(router);
+app.use("/api/v1", router);
 app.use(errorHandler);
 
 try {
