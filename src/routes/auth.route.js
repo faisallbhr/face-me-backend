2562da437211747a43044d354f54f83fd33b83c5
Faisal Bahari <fbhr1112@gmail.com>
2024-03-17 18:50:52 +0700

    fix(route): delete auth in logout and refresh token route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const { authController } = require("../controllers");
-const auth = require("../middlewares/auth");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/logout", auth, authController.logout);
-router.post("/refresh-tokens", auth, authController.refreshAuth);
+router.post("/logout", authController.logout);
+router.post("/refresh-tokens", authController.refreshAuth);
 
 module.exports = router;
